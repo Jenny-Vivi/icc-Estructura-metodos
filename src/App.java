38a87a43ede5781c25736a4c1e964cc8487dea7d
Viviana Carchi <jcarchif@est.ups.edu.ec>
2024-04-11 17:57:02 -0500

    first commit

--- src/App.java
+++ src/App.java
@@ -1,14 +1,82 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-       //crear la instancia de la clase u objeto)(clase u objeto)Metodosordenamiento
-       MetodosOrdenamiento metodosOrdenamiento=new MetodosOrdenamiento();
-       int[]numeros={30,7,12,5};
-       //Mandar imprimir un arrglo
-       int[]arregloOrdenadoBuble=metodosOrdenamiento.sortByBubble(numeros,true);
-       //Mandar imprimir mi nuevo arreglo 
-       metodosOrdenamiento.imprime((arregloOrdenadoBuble));
-       System.out.println("-Si es mayor asi que cambio");
-       
+        Scanner scanner = new Scanner(System.in);
+        MetodosOrdenamiento metodos = new MetodosOrdenamiento();
+        int opcion;
+        int[] arregloNumeros=new int[0];
 
+        do {
+            System.out.println("\nMenu principal");
+            System.out.println("1.ingrese un arrego");
+            System.out.println("2.Ordenar arreglo");
+            System.out.println("0.Salir");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Ingrese el tamaño de su arreglo");
+                    int tamano = scanner.nextInt();
+                    arregloNumeros = new int[tamano];
+
+                    for (int i = 0; i < arregloNumeros.length; i++) {
+                        System.out.println("Ingrese el valor para " + (i + 1) + ": ");
+                        arregloNumeros[i] = scanner.nextInt();
+                    }
+                    metodos.imprime(arregloNumeros);
+                    break;
+                case 2:
+                    /// Sub menu
+                    mostrarSubMenuMetodos(scanner,metodos, arregloNumeros);
+                    break;
+                case 0:
+                    System.out.println("Saliendo del programa...\n Adios");
+                    break;
+
+                default:
+                    System.out.println("Opcion no valido . Repita");
+            }
+
+        } while (opcion != 0);
+        scanner.close();
+    }
+    public static void mostrarSubMenuMetodos(Scanner scanner, MetodosOrdenamiento metodos, int []arreglo){
+        int opcionMetodo;
+        do{
+            System.out.println("\n Escoja un metodo de Ordenamiento");
+            System.out.println("Metodo Burbuja");
+            System.out.println("Metodo Seleccion");
+            System.out.println("Metodo Insercion");
+            System.out.println("0.Regresar al MENU principal");
+            opcionMetodo=scanner.nextInt();
+            switch (opcionMetodo) {
+                case 1:
+                //ingrese A-para logs y b - sin logs
+                //String opcionLog
+                //boolean condicion
+                //if opcionLog.equuals(A) cond - true
+                //else cond- false
+                //metodos.sortByBubble(arreglo,cond)
+                    int [] arregloOrdenado=metodos.sortByBubble(arreglo, false);
+                    metodos.imprime(arregloOrdenado);
+                    
+                    break;
+                    case 2:
+                    break;
+                    case 3:
+                    break;
+                    case 0:
+                    break;
+            
+                default:
+                    break;
+            }
+
+
+
+
+
+        }while(opcionMetodo!=0);
     }
 }
